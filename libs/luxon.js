f3e2e6cf49dcbd52d7b95bfb40ba1a2ef48d9993
ApichaiTi <81065469+artijo@users.noreply.github.com>
2024-11-17 19:38:12 +0700

    Add TIME_ZONE variable to .env.example and update Luxon helper functions to use it

--- libs/luxon.js
+++ libs/luxon.js
@@ -1,11 +1,11 @@
 import { DateTime } from "luxon";
 
 // Helper Function for Luxon
-function toTimezone(date, timezone = 'Asia/Bangkok') {
+function toTimezone(date, timezone = process.env.TIME_ZONE || 'Asia/Bangkok') {
     return DateTime.fromJSDate(date).setZone(timezone).toFormat('yyyy-MM-dd HH:mm:ss');
   }
   
-  function toISO(date, timezone = 'Asia/Bangkok') {
+  function toISO(date, timezone = process.env.TIME_ZONE || 'Asia/Bangkok') {
     return DateTime.fromJSDate(date).setZone(timezone).toISO();
   }
   
