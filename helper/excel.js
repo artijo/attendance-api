7f8edab4cf3ab5d0f3c641ff4494d9fb9979353d
Flower Garden <125670874+PeeraponWWW@users.noreply.github.com>
2024-11-23 16:15:38 +0700

    เพิ่ม excel
    เพิ่ม function สำหรับอ่านไฟล์ excel และ เพิ่มข้อมูล excel นั้น ปล.ยังไม่เพิ่ม function สำหรับ edit ข้อมูลไฟล์๋ excel นั้น

---
+++ helper/excel.js
@@ -0,0 +1,84 @@
+// เรียกใช้ Module
+import reader from 'xlsx';
+
+
+function readExcel(fileName){
+    // test file ที่เตรียมไว้
+    const file = reader.readFile(`./helper/${fileName}`); // อ่านไฟล์ excel
+
+    // console.log(file) //สำหรับดู obejct
+
+    let data = []; // ตัวแปรที่เอาไว้เก็บข้อมูลใน excel
+
+    const sheets = file.SheetNames;
+
+    for (let i = 0; i < sheets.length; i++) {
+        const temp = reader.utils.sheet_to_json(
+            file.Sheets[file.SheetNames[i]]) // file.Sheets['ชีต1'] การเข้าถึง ชีตไฟล์โดยใช้ชื่อ
+        temp.forEach((res) => {
+            data.push(res) // ดันข้อมูลไปที่ data
+        })
+    }
+    
+    return data
+}
+
+function insertExcel(fileName, obejectInfo){ //ObejectInfo คือ ข้อมูลที่ user insert เพื่อเพิ่มลงไฟล์ excel ไฟล์เดิม 
+    const data = readExcel(fileName)
+    if(Object.keys(obejectInfo[0]).length != Object.keys(obejectInfo[0]).length){
+        return 0;
+    }
+
+    const dataKeys = Object.keys(data[0])
+    const objectInfoDataKeys = Object.keys(obejectInfo[0])
+
+    let i = 0; // loop count for indexFor Array
+    dataKeys.forEach((keys) => {
+        if(keys != objectInfoDataKeys[i]){ // ถ้าเกิด coloum ไม่ตรงให้ หยุดการทำงาน
+            return 0; 
+        }
+        // }else{
+        //     console.log(`${keys} ตรง`)
+        // }
+        i++
+    })
+
+    const file = reader.readFile(`./helper/${fileName}`); // อ่านไฟล์ excel
+    const workSheet = file.Sheets[file.SheetNames[0]] // file.Sheets['ชีต1'] การเข้าถึง ชีตไฟล์โดยใช้ชื่อ
+    let dataSheet = reader.utils.sheet_to_json(workSheet) // แปลง sheet เป็น json
+    obejectInfo.forEach((rows) => {
+        dataSheet.push(rows)
+    })
+
+    const updatedSheet =  reader.utils.json_to_sheet(dataSheet);
+    
+    file.Sheets[file.SheetNames[0]] = updatedSheet
+
+    reader.writeFile(file,`./helper/${fileName}` );
+
+
+    // console.log(Object.keys(data[0])) // get key object เอาไว้เช็ค ex. [ 'fName', 'lName', 'email', 'tel', 'cityzenId' ]
+}
+
+// ex. data set
+let student_data = [
+    { // ข้อมูลที่มีการแก้ไข
+        fName: 'ควย',
+        lName: 'กูดำ',
+        email: 'kuygudum@gmail.com',
+        tel: 99999999,
+        cityzenId: 1479900578276
+    },
+    {
+        fName: 'พีรพล',
+        lName: 'เล่าสุอังกูร',
+        email: 'peeraphol.l@kkumail.com',
+        tel: 855555555 ,
+        cityzenId: 1479900578214
+    }
+]
+
+
+insertExcel("studentTestWrite.xlsx",student_data)
+// readExcel("student_list(test).xlsx")
+
