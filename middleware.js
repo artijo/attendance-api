6c725df51d0a88e19c8ba8c202c7de9ec8124975
ApichaiTi <81065469+artijo@users.noreply.github.com>
2024-12-17 14:52:22 +0700

    Enhance authentication middleware to validate token and handle errors

--- middleware.js
+++ middleware.js
@@ -1,6 +1,17 @@
+import db from '../prisma/client.js';
 function isAuth(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  res.redirect('/login');
+    const token = req.cookies.token;
+    if (!token) {
+        return res.status(401).json({ message: 'Token not found' });
+    }
+    try{
+        const decoded = verifyToken(token);
+        if (!decoded) {
+            return res.status(401).json({ message: 'Invalid token' });
+        }
+        req.user = decoded;
+        next();
+    }catch(err){
+        console.error(err);
+    }
 }
