9797dd165e775ea352fc6edea23bdd5a45ce268d
ApichaiTi <81065469+artijo@users.noreply.github.com>
2025-01-15 22:43:57 +0700

    เพิ่มฟังก์ชัน getActivity เพื่อดึงข้อมูลกิจกรรมตาม UUID และปรับปรุงเส้นทาง API สำหรับกิจกรรม

--- controllers/activitiesController.js
+++ controllers/activitiesController.js
@@ -26,3 +26,30 @@ export const getAllActivitiesByType = async (req, res) => {
         console.error(error);
     };
 }
+
+export const getActivity = async (req, res) => {
+    const uuid = req.params.uuid;
+    try {
+        const activity = await db.activity.findUnique({
+            where: {
+                actId: uuid
+            },
+            include: {
+                activityType: true,
+                teacher: {
+                    include: {
+                        teacher: true
+                    }
+                },
+                actParticipate: {
+                    include: {
+                        student: true
+                    }
+                }
+            }
+        });
+        return res.json(activity)
+    } catch (error) {
+        console.error(error);
+    };
+}
