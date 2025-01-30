6573105c94a0b7c8a1e707044ec23109bb819188
Kuy gu dum dev <125670874+PeeraponWWW@users.noreply.github.com>
2025-01-30 22:36:56 +0700

    เพิ่ม seed เอาไว้ seed attendence
    วิธีใช้ code runner เอาเลย อย่าลืมสร้างปฎิทินและห้องเรียน

---
+++ prisma/seedForAttendence.js
@@ -0,0 +1,61 @@
+import db from './client.js';
+
+
+async function getStudingTime(classroomId){ // after you insert the data, in studingTime already
+    const studyTime = await db.studingTime.findMany({
+        where: {
+            timetable:{
+                classId:classroomId
+            }
+        },
+        orderBy:{
+            studingTimeDate: 'asc'
+        }
+    });
+    const studyTimeIdArray = studyTime.map((studyTime) => studyTime.studyTimeId);
+    return studyTimeIdArray;
+}
+
+async function getStudent(classroomId){
+    const student = await db.classroomMember.findMany({
+        where:{
+            classId:classroomId
+        },
+        select:{
+            stdId:true,
+        }
+    });
+    const studentIdArray = student.map((student) => student.stdId);
+    return studentIdArray;
+}
+
+async function insertAttendence() {
+    const classroomId = "ceeb4319-c4bf-4dce-8299-daa237c95472";
+    const attMethodId = "27756861-4d56-4d0a-8bcf-ec57b9f5beec";
+    const teacherId = "442db621-1ffd-4fc9-a279-d5ab2d48a509";
+    const leaderId = "c126288a-6d73-4b4b-b4de-e179d196b8f5";
+    const studentIdArray = await getStudent(classroomId);
+    const studyTimeIdArray = await getStudingTime(classroomId);
+    for(let studytimeindex = 0; studytimeindex < studyTimeIdArray.length; studytimeindex++){
+        for(let studentindex = 0; studentindex < studentIdArray.length; studentindex++){
+            await db.attendance.create({
+                data:{
+                    latitute:null,
+                    longitute:null,
+                    note:"test",
+                    operatedBy: "Admin test",
+                    tchId: teacherId,
+                    leaderId: leaderId,
+                    stdId:studentIdArray[studentindex],
+                    studingTimeId:studyTimeIdArray[studytimeindex],
+                    attTimestamp:new Date(),
+                    attStatus:"PRESENT",
+                    attMethodId:attMethodId,
+
+                }
+            });
+        }
+    }
+}
+
+insertAttendence();
