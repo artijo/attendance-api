247ae2605ce8fb88172c9f4f1c341720cf791048
Kuy gu dum dev <125670874+PeeraponWWW@users.noreply.github.com>
2025-03-04 14:39:22 +0700

    insertActivity

---
+++ prisma/insertActivity.js
@@ -0,0 +1,55 @@
+import { DateTime } from 'luxon';
+import db from './client.js';
+
+async function insertActivity() {
+
+    function getDatesInRange(startDate, endDate) {
+        let date = startDate;
+        const dates = [];
+        while (date <= endDate) {
+            dates.push(date.toISODate());
+            date = date.plus({ days: 1 });
+        }
+        // console.log(dates);
+        return dates;
+    }
+    const termId = "term-2025-1";
+    const activityId = "c06ea071-007a-4500-87b8-3d76604890ff";
+    const activity = await db.activity.findUnique({
+        where:{
+            actId:activityId
+        }
+    });
+    const actStart = DateTime.fromJSDate(activity.actDate).setZone('Asia/Bangkok');
+    const actEnd = DateTime.fromJSDate(activity.actDateEnd).setZone('Asia/Bangkok');
+    const dates = getDatesInRange(actStart, actEnd);
+    const classrooms = await db.classrooms.findMany({
+        where:{
+            termId:termId
+        },
+        include:{
+            classroomMembers:true,
+            teacher:true
+        }
+    });
+    classrooms.forEach((classroom) => {
+        const teacherId = classroom.teacher[0].tchId;
+        (classroom.classroomMembers.forEach(async (classroomMember) => { 
+            dates.forEach(async (date) => {
+                const dtDate = DateTime.fromISO(`${date}T${activity.actStartTime}:00`);
+                await db.activityParticipate.create({
+                    data:{
+                        actId:activityId,
+                        stdId:classroomMember.stdId,
+                        note:"",
+                        joinTimestamp:dtDate,
+                        operateBy:"student",
+                        teacherId:teacherId,
+                    }
+                });
+            })
+            
+        }));
+    })
+};
+insertActivity();
