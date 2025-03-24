c802d87ddb5d963f4986605302a3ccc9206eb400
ApichaiTi <81065469+artijo@users.noreply.github.com>
2025-03-24 20:29:35 +0700

    Improve email sending function to handle errors and update sender address

--- libs/resend.js
+++ libs/resend.js
@@ -3,10 +3,16 @@ import { Resend } from 'resend';
 const resend = new Resend(process.env.RESEND_EMAIL_API);
 
 export async function sendEmail(to, subject, html) {
-    await resend.emails.send({
-        from: 'nps@resend.dev',
+    const {data,error} = await resend.emails.send({
+        from: 'NPS <no-reply@email.art-ohm.space>',
         to: [to],
         subject: subject,
         html: html
 });
+
+    if(error){
+        console.error(error);
+        return error;
+    }
+    return data;
 }
