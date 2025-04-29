481577c21ef3caa5520c89b0dddeaf3a0ac1b530
ApichaiTi <81065469+artijo@users.noreply.github.com>
2025-04-29 10:42:51 +0700

    feat: add classroom and members retrieval endpoints for leaders

--- controllers/leaderController.js
+++ controllers/leaderController.js
@@ -1,4 +1,5 @@
 import db from '../prisma/client.js';
+import { DateTime } from 'luxon';
 
 export function getAllLeaders(req, res) {
     try {
@@ -13,5 +14,61 @@ export function getAllLeaders(req, res) {
         });
     } catch (error) {
         console.error(error);
+    }
+}
+
+export async function getClassroomBystdId(req, res) {
+    const stdId = req.user.id;
+    try {
+        const leader = await db.leader.findFirst({
+            where: {
+                stdId: stdId
+            },
+            include: {
+                classroom: {
+                    include : {
+                        classroomType: true,
+                        term: true,
+                        classroomMembers: true,
+                        teacher: true
+                    }
+                }
+            }
+        });
+        if (!leader) {
+            return res.status(404).json({ message: 'Leader not found' });
+        }
+        return res.json(leader.classroom);
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ message: 'Internal server error' });
+    }
+}
+
+export async function getClassroomMembersByClassroomId(req, res) {
+    const { classId} = req.params;
+    try {
+        const classroomMembers = await db.classroomMember.findMany({
+            where: {
+                classId: classId
+            },
+            include: {
+                student: true,
+                classroom: {
+                    include: {
+                        teacher: true,
+                        term: true,
+                        classroomType: true,
+                    }
+                }
+            }
+        });
+        if (!classroomMembers) {
+            return res.status(404).json({ message: 'Classroom members not found' });
+        }
+        return res.json(classroomMembers);
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ message: 'Internal server error' });
     }
 }
--- routers/studentRoute.js
+++ routers/studentRoute.js
@@ -9,6 +9,7 @@ import {
 } from "../controllers/timetableController.js";
 import { getAllLeaveRequestsByStudentId, getAllLeaveRequestsType, getStudingTimeByDate, CreateLeaveRequest, getLeaveRequestById, cancelLeaveRequest } from "../controllers/leaverequestController.js";
 import { studentAttendenceEnrollment } from "../controllers/attendenceController.js";
+import { getClassroomBystdId, getClassroomMembersByClassroomId } from "../controllers/leaderController.js";
 
 // ประวัติการเข้าเรียน
 router.get('/timetable', getTimetableRoleStudent);
@@ -24,5 +25,9 @@ router.get('/studingTime/:date', getStudingTimeByDate); // เวลาเรี
 router.post('/leave',uploadS3.single('leaveFile') ,CreateLeaveRequest); // สร้างการลา
 router.get('/leave/:id', getLeaveRequestById); // ประวัติการลา
 router.delete('/leave/:id', cancelLeaveRequest); // ยกเลิกการลา
+
+// leader
+router.get('/leader/classrooms', getClassroomBystdId); // ห้องเรียนของนักเรียน
+router.get('/leader/classrooms/:classId/members', getClassroomMembersByClassroomId); // สมาชิกในห้องเรียน
 
 export default router;
