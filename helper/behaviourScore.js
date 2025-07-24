2a3483bd26451ba0bf7ae646be0c508ac7add3e9
ApichaiTi <81065469+artijo@users.noreply.github.com>
2025-07-24 12:15:41 +0700

    Refactor behaviour score functions and update Prisma schema
    - Updated `decoreteBehaviourScore` function to simplify attendance status checks and adjust score calculations.
    - Enhanced `incrementBehaviourScore` function to handle odd statuses and attendance conditions.
    - Modified Prisma schema to include `behaviourScoreTransaction` model for tracking behaviour score changes.
    - Added `EnumBehaviourScore` enum to represent score increment and decrement statuses.
    - Adjusted various model fields for consistency and clarity.

--- helper/behaviourScore.js
+++ helper/behaviourScore.js
@@ -1,27 +1,52 @@
 export function decoreteBehaviourScore(behaviourScore, attendanceStatus) {
-  if (attendanceStatus.status === "ABSENT") {
+  if (attendanceStatus === "ABSENT") {
     return {
       ...behaviourScore,
-      score: behaviourScore.score - 1, // Deduct 1 point for absence
+      decorreted: true, // Mark as decorated
+      score: behaviourScore - 1, // Deduct 1 point for absence
       reason: "Attendance status is absent, score adjusted accordingly.",
     };
-  } else if (attendanceStatus.status === "LATE") {
+  } else if (attendanceStatus === "LATE") {
     return {
       ...behaviourScore,
-      score: behaviourScore.score - 0.5, // Deduct 0.5 points for being late
+      decorreted: true, // Mark as decorated
+      score: behaviourScore - 0.5, // Deduct 0.5 points for being late
       reason: "Attendance status is late, score adjusted accordingly.",
     };
   }
   return {
     ...behaviourScore,
+    decorreted: false, // No decoration needed
     reason: "No attendance issues, score remains unchanged.",
   };
 }
 
-export function incrementBehaviourScore(behaviourScore) {
-  return {
-    ...behaviourScore,
-    score: behaviourScore.score + 1,
-    reason: "Behaviour score incremented by 1.",
-  };
+export function incrementBehaviourScore(
+  behaviourScore,
+  oddStatus,
+  attendanceStatus
+) {
+  if (
+    oddStatus === "LATE" &&
+    attendanceStatus !== "ABSENT" &&
+    attendanceStatus !== "LATE"
+  ) {
+    return {
+      ...behaviourScore,
+      incremented: true, // Mark as incremented
+      score: behaviourScore + 0.5, // Increment by 0.5 for being late
+      reason: "Incremented score for being late while absent.",
+    };
+  } else if (
+    oddStatus === "ABSENT" &&
+    attendanceStatus !== "ABSENT" &&
+    attendanceStatus !== "LATE"
+  ) {
+    return {
+      ...behaviourScore,
+      incremented: true, // Mark as incremented
+      score: behaviourScore + 1, // Increment by 1 for absence
+      reason: "Incremented score for absence.",
+    };
+  }
 }
